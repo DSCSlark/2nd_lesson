783bcd1ccc6e4c3c9a7f601fc0cec5804aa7cccc
DSCSlark <9047915555@mail.ru>
2023-03-16 14:23:22 +0300

    task 3

---
+++ ДЗ_1_семинар/2nd/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на 2 без остатка).
+
+Console.WriteLine("Please, write a number");
+int n_1 = int.Parse(Console.ReadLine()!);
+
+if (n_1 % 2 == 0)
+{
+    Console.Write($"The number {n_1} is even");
+}
+else
+{
+    Console.Write($"The number {n_1} is odd");
+}
